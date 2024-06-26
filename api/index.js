57ed8d8ed7b25fdb6ca2757a1663ace8a164e7ff
Jonathan Muhire <muhirejonathan123@gmail.com>
2024-06-26 15:01:47 -0700

    resolve json package scripts configuration

--- api/index.js
+++ api/index.js
@@ -6,7 +6,6 @@ import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
-import cors from 'cors';
 import { fileURLToPath } from 'url';
 
 dotenv.config();
@@ -27,8 +26,6 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
-
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
